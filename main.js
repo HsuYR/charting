e18a273892fc0416fdec309575003b3bf5194592
HsuYR <hyr1502@gmail.com>
2021-02-03 17:29:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,86 @@
 const app = Vue.createApp({
     data() {
         return {
-            content:{
-                patientName: 'John Doe',
+            patient:{
+                name: '',
+                sex: undefined,
+                events: [
+                    {}
+                ],
+                problems: [
+                    {}
+                ]
             },
             inputJSON: ''
         }
     },
     computed: {
         outputJSON() {
-            return JSON.stringify(this.content)
+            return btoa(encodeURIComponent(JSON.stringify(this.patient)))
+            //return JSON.stringify(this.patient)
+            //return lzw_encode(encodeURIComponent(JSON.stringify(this.patient)))
         }
     },
     methods: {
         loadJSON() {
-            this.content = JSON.parse(this.inputJSON)
+            this.patient = JSON.parse(decodeURIComponent(atob(this.inputJSON)))
+            //this.patient = JSON.parse(this.inputJSON)
+            //this.patient = JSON.parse(decodeURIComponent(lzw_decode(this.inputJSON)))
         }
     }
 })
 
 const vm = app.mount('#app')
+
+// LZW-compress a string
+function lzw_encode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var out = [];
+    var currChar;
+    var phrase = data[0];
+    var code = 256;
+    for (var i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
+    }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+    for (var i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var currChar = data[0];
+    var oldPhrase = currChar;
+    var out = [currChar];
+    var code = 256;
+    var phrase;
+    for (var i=1; i<data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            phrase = data[i];
+        }
+        else {
+           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+        }
+        out.push(phrase);
+        currChar = phrase.charAt(0);
+        dict[code] = oldPhrase + currChar;
+        code++;
+        oldPhrase = phrase;
+    }
+    return out.join("");
+}
